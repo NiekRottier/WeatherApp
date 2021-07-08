f08c1b9fe0dec0576ce627a973b7f178927e0159
NiekRottier <niekrottier@gmail.com>
2021-07-08 02:20:01 +0200

    Styled settingspage

--- pages/SettingsScreen.js
+++ pages/SettingsScreen.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import NavigationBar from '../components/NavigationBar';
-import { MMKV } from 'react-native-mmkv';
 
 const SettingsScreen = ({navigation}) => {
   // Standard location is NL, Flushing
@@ -19,9 +18,11 @@ const SettingsScreen = ({navigation}) => {
   return (
     <View style={styles.view}>
       <View style={styles.mainContent}>
+        <Text style={styles.title}>Set location</Text>
         <View style={styles.inputContainer}>
-          <Text>Latitude:</Text>
+          <Text style={styles.label}>Latitude:</Text>
           <TextInput
+            style={styles.input}
             placeholder='Enter latitude here'
             keyboardType="numeric"
             onChangeText={(lat) => changeValue('lat', lat)}
@@ -29,8 +30,9 @@ const SettingsScreen = ({navigation}) => {
           />
         </View>
         <View style={styles.inputContainer}>
-          <Text>Longitude:</Text>
+          <Text style={styles.label}>Longitude:</Text>
           <TextInput
+            style={styles.input}
             placeholder='Enter longitude here'
             keyboardType="numeric"
             onChangeText={(lon) => changeValue('lon', lon)}
@@ -49,16 +51,38 @@ const styles = StyleSheet.create({
     height: '100%',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingTop: '5%'
+    paddingTop: '5%',
+    backgroundColor: '#f3f3f3'
   },
   mainContent: {
     height: '92%',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     alignItems: 'center',
   },
   inputContainer: {
-    justifyContent: 'center',
+    height: '20%',
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 40
+  },
+  label: {
+    width: '30%',
+    textAlign: 'right',
+    fontSize: 20,
+    color: '#404040'
+  },
+  input: {
+    width: '65%',
+    fontSize: 20,
+    backgroundColor: '#83EAF5',
+    borderBottomColor: '#78A9FA',
+    borderBottomWidth: 2,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
   }
 });
 
